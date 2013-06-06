dfbb8e29a73b00ed6d7a23e7c50b4ef66a861cb8
Yuki Ito <yuki@gnnk.net>
2013-06-06 22:06:40 +0900

    Implement

--- lib/random.ex
+++ lib/random.ex
@@ -1,2 +1,66 @@
 defmodule Random do
+  def seed do
+    :random.seed
+  end
+
+  def seed(x, y, z) do
+    :random.seed(x, y, z)
+  end
+
+  def seed({ x, y, z }) do
+    :random.seed(x, y, z)
+  end
+
+  def default_seed do
+    :random.seed0
+  end
+
+  def random do
+    :random.uniform
+  end
+
+  def random(n) when is_integer(n) and n > 0 do
+    :random.uniform(n) - 1
+  end
+
+  def random(n) when is_integer(n) do
+    0
+  end
+
+  def random(n) when is_float(n) do
+    random trunc(n)
+  end
+
+  def sample([]) do
+    nil
+  end
+
+  def sample(list) when is_list(list) do
+    :lists.nth(:random.uniform(length(list)), list)
+  end
+
+  def sample(range) when is_range(range) do
+    list = Enum.to_list(range)
+    sample(list)
+  end
+
+  def sample(list, n) when is_list(list) and is_integer(n) and n >= 0 do
+    list = lc x inlist list, do: { :random.uniform, x }
+    lc { _, x } inlist list |> :lists.sort |> :lists.sublist(n), do: x
+  end
+
+  def sample(range, n) when is_range(range) and is_integer(n) and n >= 0 do
+    list = Enum.to_list(range)
+    sample(list, n)
+  end
+
+  def shuffle(list) when is_list(list) do
+    list = lc x inlist list, do: { :random.uniform, x }
+    lc { _, x } inlist list |> :lists.sort, do: x
+  end
+
+  def shuffle(range) when is_range(range) do
+    list = Enum.to_list(range)
+    shuffle(list)
+  end
 end
--- test/random_test.exs
+++ test/random_test.exs
@@ -3,7 +3,92 @@ Code.require_file "test_helper.exs", __DIR__
 defmodule RandomTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  test "random" do
+    Enum.each 0..10000, fn(_) ->
+      x = Random.random
+      assert x >= 0.0 && x <= 1.0
+    end
+  end
+
+  test "random integer" do
+    Enum.each 0..10000, fn(_) ->
+      assert Random.random(3) in [0,1,2]
+    end
+  end
+
+  test "random negative" do
+    Enum.each 0..10000, fn(_) ->
+      assert Random.random(-1) == 0
+    end
+  end
+
+  test "random float" do
+    Enum.each 0..10000, fn(_) ->
+      assert Random.random(3.2) in [0,1,2]
+    end
+  end
+
+  test "sample nonempty list" do
+    Enum.each 0..10000, fn(_) ->
+      assert Random.sample([0,1,2]) in [0,1,2]
+    end
+  end
+
+  test "sample empty list" do
+    assert Random.sample([]) == nil
+  end
+
+  test "sample range" do
+    Enum.each 0..10000, fn(_) ->
+      assert Random.sample(1..3) in [1,2,3]
+    end
+  end
+
+  test "sample list returns list" do
+    Enum.each 0..10000, fn(_) ->
+      list = Random.sample([0,1,2], 2)
+      assert list |> Enum.sort |> Enum.all? &1 in [0,1,2]
+      assert list |> Enum.uniq |> Enum.count == list |> Enum.count
+    end
+
+    Enum.each 0..10000, fn(_) ->
+      list = Random.sample([0,1,2], 4)
+      assert list |> Enum.sort == [0,1,2]
+    end
+  end
+
+  test "sample range returns list" do
+    Enum.each 0..10000, fn(_) ->
+      list = Random.sample(1..3, 2)
+      assert list |> Enum.sort |> Enum.all? &1 in [1,2,3]
+      assert list |> Enum.uniq |> Enum.count == list |> Enum.count
+    end
+
+    Enum.each 0..10000, fn(_) ->
+      list = Random.sample(1..3, 4)
+      assert list |> Enum.sort == [1,2,3]
+    end
+  end
+
+  test "sample with negative number" do
+    assert_raise FunctionClauseError, "no function clause matching in Random.sample/2", fn ->
+      Random.sample([0,1,2], -1)
+    end
+  end
+
+  test "shuffle nonempty list" do
+    Enum.each 0..10000, fn(_) ->
+      assert Random.shuffle([0,1,2]) |> Enum.sort == [0,1,2]
+    end
+  end
+
+  test "shuffle empty list" do
+    assert Random.shuffle([]) == []
+  end
+
+  test "shuffle range" do
+    Enum.each 0..10000, fn(_) ->
+      assert Random.shuffle(1..3) |> Enum.sort == [1,2,3]
+    end
   end
 end
